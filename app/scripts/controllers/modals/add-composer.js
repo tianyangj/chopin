ebc30d13c6c83806fc5653c8d361c24479c2c595
Tommy Jiang <tianyangj@gmail.com>
2015-05-19 22:37:56 -0700

    composer data service

--- app/scripts/controllers/modals/add-composer.js
+++ app/scripts/controllers/modals/add-composer.js
@@ -1,11 +1,26 @@
 'use strict';
 
-angular.module('lilybook').controller('AddComposerCtrl', function ($location) {
-	
-	this.root = $location.protocol() + '://' + $location.host() + '/';
-	
-	this.submit = function() {
-		console.log(this);	
+angular.module('lilybook').controller('AddComposerCtrl', function ($location, $modalInstance, composerSvc) {
+
+	var that = this;
+
+	that.root = $location.protocol() + '://' + $location.host() + '/';
+
+	that.submit = function () {
+		composerSvc.getComposer(that.vanity).then(function (composer) {
+			if (!composer) {
+				composerSvc.createComposer({
+					vanity: that.vanity,
+					fullname: that.fullname,
+					image: that.image,
+					description: that.description
+				}).then(function () {
+					$modalInstance.close();
+				});
+			} else {
+				alert('vanity exist!');
+			}
+		});
 	};
-	
+
 });
---
+++ app/scripts/services/composer.js
@@ -0,0 +1,36 @@
+/* global Firebase */
+
+'use strict';
+
+angular.module('lilybook').factory('composerSvc', function ($q) {
+
+  var ref = new Firebase('https://chopin.firebaseio.com/composers');
+
+  var createComposer = function (composer) {
+    var defer = $q.defer();
+    ref.child(composer.vanity).set(composer, function (error) {
+      if (error) {
+        defer.reject(error);
+      } else {
+        defer.resolve(composer);
+      }
+    });
+    return defer.promise;
+  };
+
+  var getComposer = function (vanity) {
+    var defer = $q.defer();
+    ref.child(vanity).on('value', function (snapshot) {
+      defer.resolve(snapshot.val());
+    }, function (error) {
+        defer.reject(error);
+      });
+    return defer.promise;
+  };
+
+  return {
+    createComposer: createComposer,
+    getComposer: getComposer
+  };
+
+});
