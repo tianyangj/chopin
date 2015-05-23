ab68797e85ced811153be51c3be15ba76533263d
Tommy Jiang <tianyangj@gmail.com>
2015-05-22 22:08:24 -0700

    userSvc remove firebase

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -5,30 +5,6 @@
 
 angular.module('lilybook').factory('userSvc', function ($q) {
 
-  var ref = new Firebase('https://chopin.firebaseio.com/users');
-
-  var createUser = function (user) {
-    var defer = $q.defer();
-    ref.child(user.uid).set(user, function (error) {
-      if (error) {
-        defer.reject(error);
-      } else {
-        defer.resolve(user);
-      }
-    });
-    return defer.promise;
-  };
-
-  var getUser = function (uid) {
-    var defer = $q.defer();
-    ref.child(uid).on('value', function (snapshot) {
-      defer.resolve(snapshot.val());
-    }, function (error) {
-        defer.reject(error);
-      });
-    return defer.promise;
-  };
-
   var current = function () {
     var defer = $q.defer();
     var user = Parse.User.current();
@@ -90,8 +66,6 @@ angular.module('lilybook').factory('userSvc', function ($q) {
   };
 
   return {
-    createUser: createUser,
-    getUser: getUser,
     current: current,
     signUp: signUp,
     logIn: logIn,
