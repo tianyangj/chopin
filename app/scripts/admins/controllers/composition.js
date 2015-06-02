271becac9da088c521212f7a17a1bc829cbfcc22
Tommy Jiang <tianyangj@gmail.com>
2015-06-02 01:12:48 -0700

    disable videos and sheets tabs if necessary

--- app/scripts/admins/controllers/composition.js
+++ app/scripts/admins/controllers/composition.js
@@ -3,4 +3,8 @@
 angular.module('lilybook').controller('AdminCompositionCtrl', function (composition) {
 
 	this.composition = composition;
+
+	this.hasId = function () {
+		return composition === undefined || composition === null;
+	};
 });
